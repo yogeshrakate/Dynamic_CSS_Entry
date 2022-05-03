84b5e08fda29cdcee5e883ef54ac7d8c14b7615d
Yogesh Rakate <65539826+yogeshrakate@users.noreply.github.com>
2022-05-03 16:03:58 +0530

    Update views.py

--- percentagebox/boxtrial/views.py
+++ percentagebox/boxtrial/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    px_size = 400
-    return render(request, 'index.html', {'px_size':px_size})
+    px_sizes = 4
+    px_size_list = [200,300,400,500]
+    return render(request, 'index.html', {'px_sizes':px_sizes, 'px_size_list':px_size_list})
